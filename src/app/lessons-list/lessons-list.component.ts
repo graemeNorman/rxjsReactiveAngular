1d9f90b84c374dbf3492a5da27e8d4b29a45f7fc
graemeNorman <graemenorman@tengroup.com>
2017-05-03 11:17:41 +0100

    Added interface which watches for changes to the lessons component

---
+++ src/app/lessons-list/lessons-list.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { globalEventBus, Observer } from "../event-bus-experiments/event-bus";
+import { Lesson } from "../shared/model/lesson";
+
+@Component({
+  selector: 'lessons-list',
+  templateUrl: './lessons-list.component.html',
+  styleUrls: ['./lessons-list.component.css']
+})
+export class LessonsListComponent implements Observer {
+
+  lessons: Lesson[] = [];
+
+  constructor() {
+    console.log('Lesson list component is registered as observer...');
+    globalEventBus.registerObserver(this);
+  }
+
+  // ngOnInit() { }
+
+  notify(data: Lesson[]) {
+    console.log('Lessons list component received data');
+    this.lessons = data;
+  }
+
+}
---
+++ src/app/shared/model/test-lesson.ts
@@ -0,0 +1,19 @@
+import { Lesson } from "./lesson";
+
+export const testLessons: Lesson[] = [
+  {
+    id: 1,
+    description: 'Setting up an Angular 2 Development Environment',
+    duration: '5:00'
+  },
+  {
+    id: 2,
+    description: 'Running the Lessons Code',
+    duration: '7:00'
+  },
+  {
+    id: 3,
+    description: 'Build your first APP - Hello World Step by Step',
+    duration: '9:00'
+  }
+];
